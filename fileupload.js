6593581ad5e972b0020e9a98055588020a115830
Colin Carter <colin@calltech.co.uk>
2009-05-07 10:22:41 +0100

    Added gitignore Changed index.html to index.php

--- fileupload.js
+++ fileupload.js
@@ -1,3 +1,16 @@
 (function() {
+  Ext.namespace('Ext.ux');
+  
+  Ext.ux.FileUpload = function(config) {
+    Ext.apply(this, config);
+    
+    this.panel = {
+    };
+    
+    Ext.ux.FileUpload.superclass.constructor.call(this, this.panel);
+  };
+  
+  Ext.extend(Ext.ux.FileUpload, Ext.Panel, {
+  });
 
 })();
--- index.php
+++ index.php
@@ -6,11 +6,15 @@
   <script src="fileupload.js" type="text/javascript"></script>
   <script type="text/javascript">
     Ext.onReady(function() {
-      
+      var fileUpload = new Ext.ux.FileUpload({
+        el:       'upload-div',
+        progress: 'progress.php',
+        uploadTo: 'upload.php'
+      });
     });
   </script>
 </head>
 <body>
-
+  <div id="upload-div"><!--upload panel will go here --></div>
 </body>
 </html>
